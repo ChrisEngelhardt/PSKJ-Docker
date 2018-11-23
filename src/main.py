45f3ce04847224a096284f3780c2df8759f21c2c
Chris Engelhardt <chris.engelhardt@peakerest.com>
2018-11-23 08:22:54 +0000

    Update src/main.py

--- src/main.py
+++ src/main.py
@@ -1,22 +1,3 @@
 #!/usr/bin/python
 
-import util
-import pdfLoader
-
-
-outputFolder = "../output/"
-datasetFolder = "../dataset/"
-templateFolder = datasetFolder + "templates/"
-imagsFolder = datasetFolder + "images/"
-handwrittenFolder = datasetFolder + "handwritten/"
-machinePrintedFolder = datasetFolder + "machinePrinted/"
-
-
-print("Clean output")
-util.cleanupOutput(outputFolder)
-
-print("Loading background template")
-page = pdfLoader.getFirstPageOfPDF(templateFolder + "a4-graph-5mm.pdf")
-page.save(outputFolder + "/test.jpg", 'JPEG')
-
-print("Finished")
+print("Hello World")
